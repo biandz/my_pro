65d251799ef9f02551bea37f515bbcb6717abac4
biandz <biandz0917@163.com>
2023-06-07 14:24:01 +0800

    first web server

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("hahahah"))
-	})
-	http.ListenAndServe(":8888", nil)
+	http.HandleFunc("/", IndexController)
+	_ = http.ListenAndServe(":8888", nil)
+}
+
+func IndexController(writer http.ResponseWriter, request *http.Request) {
+	m := make(map[string]interface{})
+	m["code"] = 0
+	m["message"] = "success"
+	m["data"] = nil
+	marshal, err := json.Marshal(m)
+	if err != nil {
+		return
+	}
+	_, _ = writer.Write(marshal)
 }
